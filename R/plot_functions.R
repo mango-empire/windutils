e8daa89efb225d931d638d83ec6185e688fc830d
Kevin Eng <kevine1221@gmail.com>
2025-04-07 14:54:20 -0400

    +

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,3 +1,58 @@
-plot_diurnal_median <- function(wind_data) {
-    wind_data <- preprocess_wind(wind_data)
+#' Title
+#'
+#'
+#' @import dplyr
+#' @import ggplot2
+#'
+#' @export
+#'
+plot_wspd_median <- function(wind_data) {
+
+    wind_data |>
+        summarise_10_median(wspd_vec_mean) |>
+        ggplot(aes(time, mean_median_wspd_vec_mean)) +
+            geom_point(shape = 1) +
+            ggtitle("Wind Speed (m/s)") +
+            ylab("median") +
+            xlab("hour (UTC)") +
+            theme_minimal()
+}
+
+#' Title
+#'
+#' @import dplyr
+#' @import ggplot2
+#'
+#' @export
+#'
+plot_wspd_max <- function(wind_data) {
+
+    wind_data |>
+        mutate_east_north() |>
+        group_by(minute_of_day) |>
+        summarise(max_wspd = max(wspd_vec_mean, na.rm = TRUE)) |>
+        mutate(hour = minute_of_day / 60) |>
+        ggplot(aes(hour, max_wspd)) +
+            geom_point() +
+            theme_minimal() +
+            xlim(0, 24)
+
+}
+
+#' Title
+#'
+#'
+#' @import dplyr
+#' @import ggplot2
+#'
+#' @export
+#'
+plot_cycle_median <- function(wind_data) {
+
+    wind_data |>
+        mutate_east_north() |>
+        summarise_10_median(easterly, northerly) |>
+        ggplot(aes(mean_median_northerly, mean_median_easterly)) +
+            geom_path() +
+            theme_minimal()
 }
--- R/preprocess.R
+++ R/preprocess.R
@@ -1,11 +1,44 @@
-preprocess_wind <- function(wind_data) {
-    df_out <- wind_data |>
-        filter(month(time) == 6) |>
+#' Title
+#'
+#'
+#' @import dplyr
+#' @import lubridate
+#'
+#' @export
+#'
+mutate_east_north <- function(wind_data) {
+    wind_data |>
         mutate(minute_of_hour = minute(time),
                hour = hour(time),
                minute_of_day = 60*hour + minute_of_hour) |>
         mutate(wdir_rad = wdir_vec_mean * (pi / 180),
                northerly = wspd_vec_mean * sin(wdir_rad),
                easterly = wspd_vec_mean * cos(wdir_rad))
-    df_out
 }
+
+
+#' Title
+#'
+#'
+#' @import dplyr
+#' @import lubridate
+#'
+#' @export
+#'
+summarise_10_median <- function(wind_data, ...) {
+    vars <- rlang::ensyms(...)
+
+    wind_data |>
+        mutate_east_north() |>
+        group_by(time = as_hms(time)) |>
+        summarise(across(all_of(as.character(vars)), ~ median(.x, na.rm = TRUE), .names = "median_{.col}")) |>
+        mutate(time = as_datetime("2025-04-06") + time) |>
+        group_by(time_grp_10 = floor_date(time, "10 mins")) |>
+        summarise(across(starts_with("median_"), mean, .names = "mean_{.col}")) |>
+        mutate(time_grp_10 = hms::as_hms(time_grp_10)) |>
+        rename(time = time_grp_10)
+}
+
+
+
+
